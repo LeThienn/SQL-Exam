f8860a3700816b0a3b2aea4fc141773977f68fd2
LeThienIT <55796074+LeThienn@users.noreply.github.com>
2022-06-07 10:28:09 +0700

    add file sql

---
+++ Bài 2/baithuchanh.sql
@@ -0,0 +1,38 @@
+-- NoIndex
+SELECT *INTO dbo.DimProduct_NoIndex FROM dbo.DimProduct
+SELECT ProductKey, ProductAlternateKey FROM dbo.DimProduct_NoIndex WHERE ProductKey = 600
+
+-- Index
+SELECT *INTO dbo.DimProduct_Index FROM dbo.DimProduct GO 
+CREATE INDEX Idx_DimProduc_Index_ProductKey ON dbo.DimProduct_Index(ProductKey)
+
+SELECT ProductKey, ProductAlternateKey FROM dbo.DimProduct_Index WHERE ProductKey = 600
+
+-- CLUSTERED
+
+SELECT * INTO dbo.ProspectiveBuyer_CLUSTERED FROM dbo.ProspectiveBuyer GO
+CREATE CLUSTERED INDEX AX_ProspectiveBuyer_ProspectiveBuyerKey
+ON dbo.ProspectiveBuyer (ProspectiveBuyerKey)
+
+-- NONCLUSTERED
+SELECT * INTO dbo.ProspectiveBuyer_NONCLUSTERED FROM dbo.ProspectiveBuyer GO
+CREATE NONCLUSTERED INDEX IX_ProspectiveBuyer_ProspectiveBuyerKey
+ON dbo.ProspectiveBuyer (ProspectiveBuyerKey)
+
+USE AdventureWorksDW 
+GO
+SET STATISTICS TIME ON
+GO
+SELECT ProspectiveBuyerKey, ProspectAlternateKey FROM dbo.ProspectiveBuyer_NONCLUSTERED WHERE ProspectiveBuyerKey = 978
+GO
+SET STATISTICS IO OFF
+GO
+
+USE AdventureWorksDW 
+GO
+SET STATISTICS IO ON
+GO
+SELECT ProspectiveBuyerKey, ProspectAlternateKey FROM dbo.ProspectiveBuyer_CLUSTERED WHERE ProspectiveBuyerKey = 978
+GO
+SET STATISTICS IO OFF
+GO
